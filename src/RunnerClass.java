90206c5d6f421e972dd476c6bc9a8d92d6ba22d1
RhymeLyet <rhymeinparentheses@gmail.com>
2016-11-20 17:26:55 -0800

    gg
    gg

---
+++ src/RunnerClass.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+public class RunnerClass {
+JFrame frame;
+static final int width = 500;
+static final int height = 800;
+public static void main(String[] args) {
+
+}
+	RunnerClass(){
+		
+	}
+	void setup(){
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		}
+}
